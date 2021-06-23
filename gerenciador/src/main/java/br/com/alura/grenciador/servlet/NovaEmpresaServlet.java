3cb8b0db81ee118886e199539100209f215108f0
Cleonildo Soares Guimaraes Junior <cleonildo.junior@b2wdigital.com>
2021-06-23 16:52:49 -0300

    Get e Post

---
+++ gerenciador/src/main/java/br/com/alura/grenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,33 @@
+package br.com.alura.grenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando novas empresas");
+		PrintWriter out = response.getWriter();
+		String nomeEmpresa = request.getParameter("nome");
+		out.println(
+				"<html><body>" + nomeEmpresa+ " Empresa Cadastrada</body></html>"
+				
+				);
+		
+	}
+
+}
