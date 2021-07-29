937257600220c75be206ab08de3b180bfeeaa7d7
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-07-28 22:01:29 -0300

    web.xml e finalizacao

--- gerenciador/src/main/java/br/com/alura/grenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/main/java/br/com/alura/grenciador/servlet/OiMundoServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
+//@WebServlet(urlPatterns = "/oi")
 public class OiMundoServlet extends HttpServlet{
 	
 	@Override
