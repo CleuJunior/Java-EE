5122d84cba64eba32362a5fc1ab809881b46ffaa
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-07-30 23:44:55 -0300

    Xpath

--- xml-com-java/src/br/com/alura/Teste/Sistema.java
+++ xml-com-java/src/br/com/alura/Teste/Sistema.java
@@ -2,6 +2,10 @@ package br.com.alura.Teste;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -22,15 +26,24 @@ public class Sistema {
 		Element venda = document.getDocumentElement();
 		String moeda = venda.getAttribute("moeda");
 		System.out.println(moeda);
-		NodeList produtos = document.getElementsByTagName("produto");
 		
-		for (int i = 0; i < produtos.getLength(); i++) {
-			Element produto = (Element) produtos.item(i);
-			String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
-			double preco = Double.parseDouble(produto.getElementsByTagName("preco").item(0).getTextContent());
-			
-			Produto prod = new Produto(nome, preco);			
-			System.out.println(prod);
+		String exp = "/venda/produtos/produto[contains(nome, 'Livro')]";
+		XPath path = XPathFactory.newInstance().newXPath();
+		XPathExpression expression = path.compile(exp);
+
+
+		NodeList produtos = (NodeList) expression.evaluate(document, XPathConstants.NODESET);
+		
+		for(int i = 0;i < produtos.getLength();i++) {
+		    Element produto = (Element) produtos.item(i);
+		    String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+
+		    
+	        double preco = Double.parseDouble(produto.getElementsByTagName("preco").item(0).getTextContent());
+	        Produto prod = new Produto(nome, preco);
+	        System.out.println(prod);
+		    
+
 		}
 
 
