884f24d3126833d5ec48e4afb84cf81682a85bb4
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-08-02 22:16:55 -0300

    Complemento do sistema

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,18 +1,46 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	
+    private Integer autorId;
+
+    public void setAutorId(Integer autorId) {
+        this.autorId = autorId;
+    }
+
+    public Integer getAutorId() {
+        return autorId;
+    }
 
 	public Livro getLivro() {
 		return livro;
 	}
+	
+    public List<Autor> getAutoresDoLivro() {
+        return this.livro.getAutores();
+    }
+	
+    public List<Autor> getAutores() {
+        return new DAO<Autor>(Autor.class).listaTodos();
+    }
+    
+    public void gravarAutor() {
+        Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+        this.livro.adicionaAutor(autor);
+    }
 
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
